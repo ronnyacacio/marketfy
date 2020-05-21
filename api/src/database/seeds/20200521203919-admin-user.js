e8382bd41668fd1144c92e89a86ed35521959160
Ronny Acacio <ronnyacacio27@gmail.com>
2020-05-21 17:43:50 -0300

    migration create-users and seed admin

---
+++ api/src/database/seeds/20200521203919-admin-user.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (QueryInterface) => {
+    return QueryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Comercial RND',
+          email: 'admin@gmail.com',
+          password_hash: bcrypt.hashSync('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: () => {},
+};
